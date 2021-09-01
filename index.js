5c796a5f3e5e435602036daeed32cab0c324f653
Ismail <mailismailpe@gmail.com>
2021-09-01 18:30:18 +0530

    added express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('welcome to reportgenerator backend api'));
